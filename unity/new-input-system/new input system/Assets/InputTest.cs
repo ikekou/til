bfb2d5056eef0872b42da5ebf335115ab9a764ba
Kousei Ikeda <mail@ikekou.jp>
2019-07-09 00:25:16 +0900

    unity/new-input-system

---
+++ unity/new-input-system/new input system/Assets/InputTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Input;
+
+public class InputTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogFormat("Mouse Position : {0}", Mouse.current.position.ReadValue());
+        Debug.LogFormat("Press Space Key : {0}", Keyboard.current.spaceKey.isPressed);
+    }
+}
