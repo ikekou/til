0ccc0349001800ea33747df04e9d157910eef59c
Kousei Ikeda <mail@ikekou.jp>
2019-07-09 00:27:33 +0900

    unity/UniRx

---
+++ unity/UniRx/README.md
@@ -0,0 +1,7 @@
+neuecc/UniRx: Reactive Extensions for Unity https://github.com/neuecc/UniRx
+
+リアクティブスパゲティを避けるための2つの原則 │ Aiming 開発者ブログ https://developer.aiming-inc.com/programming/avoid-rx-spaghetti/
+
+UniRx入門 その1 - Qiita https://qiita.com/toRisouP/items/2f1643e344c741dd94f8
+
+Asset Store https://www.assetstore.unity3d.com/#!/content/17276
---
+++ unity/UniRx/UniRx/Assets/TimeCounter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerCounter : MonoBehaviour
+{
+    public delegate void TimerEventHandler(int time);
+
+    public event TimerEventHandler OnTimeChanged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TimerCoroutine());
+    }
+
+    IEnumerator TimerCoroutine()
+    {
+        var time = 100;
+        while (time > 0)
+        {
+            time--;
+
+            OnTimeChanged(time);
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
