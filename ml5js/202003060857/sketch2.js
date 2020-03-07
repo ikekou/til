6ab33ee67b8f4ea943e1a35f1cebc036e8eb6481
Kousei Ikeda <mail@ikekou.jp>
2020-03-07 15:59:21 +0900

    ml5js

--- ml5js/202003060857/sketch2.js
+++ ml5js/202003060857/sketch2.js
@@ -7,3 +7,68 @@ p5.js Web Editor | FaceApi_Image_Landmarks https://editor.p5js.org/ml5/sketches/
 
 */
 
+let FaceApi;
+let img;
+let detections;
+
+const detection_options={
+    whtLandscape:true,
+    withDescriptors:false,
+}
+
+function preload(){
+    // img = loadImage('frida.jpg')
+    img = loadImage('okawa.png')
+}
+
+function setup(){
+    createCanvas(400,400);
+    img.resize(width,height);
+
+    faceapi = ml5.faceApi(detection_options, modelReady);
+    textAlign(RIGHT);
+}
+
+function modelReady(){
+    faceapi.detectSingle(img,gotResult);
+}
+
+function gotResult(err,result){
+    if(err){
+        console.log(err);
+        return;
+    }
+
+    detections = result;
+
+    background(255);
+
+    image(img,0,0,width,height);
+    if(detections){
+        drawBox(detections);
+        drawLandmarks(detections);
+    }
+}
+
+function drawBox(detections){
+    const alignedRect = detections.alignedRect;
+    const {_x,_y,_width,_height} = alignedRect._box;
+    noFill();
+    stroke(161,95,251);
+    strokeWeight(2);
+    rect(_x,_y,_width,_height);
+}
+
+function drawLandmarks(){
+    noFill();
+    stroke(161,95,251);
+    strokeWeight(2);
+
+    push();
+
+    beginShape();
+    detections.parts.mouth.forEach(item=>{
+        vertex(item._x,item._y);
+    })
+    endShape(CLOSE);
+}
